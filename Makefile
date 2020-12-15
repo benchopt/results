4bb59ed2f26c4266ce903f02abe931aac8e47c04
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-12-15 22:46:08 +0100

    nicer

--- Makefile
+++ Makefile
@@ -5,11 +5,17 @@ OUTPUTDIR=$(BASEDIR)/rendered
 
 GITHUB_PAGES_BRANCH=gh-pages
 
+.PHONY: clean render publish
+
+all: render
+
+clean:
+	rm -rf build
+
 render:
 	${PYTHON} main.py
 
-publish: render
+publish:
 	touch $(OUTPUTDIR)/.nojekyll
 	ghp-import -m "Generate site" -b $(GITHUB_PAGES_BRANCH) $(OUTPUTDIR)
 	git push origin $(GITHUB_PAGES_BRANCH)
-
--- main.py
+++ main.py
@@ -15,14 +15,17 @@ from benchopt.viz import PLOT_KINDS
 matplotlib.use('Agg')
 
 ROOT = Path(__file__).parent
-OUTPUT_DIR = Path(__file__).parent / "rendered"
-OUTPUT_BENCH_DIR = OUTPUT_DIR / "outputs"
-OUTPUT_FIG_DIR = OUTPUT_DIR / "figures"
-TEMPLATE = ROOT / "templates" / "index.mako.html"
+BUILD_DIR = Path(__file__).parent / "build"
+BUILD_DIR_OUTPUTS = BUILD_DIR / "outputs"
+BUILD_DIR_FIGURES = BUILD_DIR / "figures"
+
+TEMPLATE_INDEX = ROOT / "templates" / "index.mako.html"
+TEMPLATE_BENCHMARK = ROOT / "templates" / "benchmark.mako.html"
 TEMPLATE_RESULT = ROOT / "templates" / "result.mako.html"
+
+JQUERY_JS = ROOT / "resources" / "jquery.js"
 JQUERY_DATATABLES_JS = ROOT / "resources" / "jquery.dataTables.min.js"
 JQUERY_DATATABLES_CSS = ROOT / "resources" / "jquery.dataTables.min.css"
-JQUERY_JS = ROOT / "resources" / "jquery.js"
 JQUERY_DATATABLES_JS = ROOT / "resources" / "jquery.dataTables.min.js"
 JQUERY_DATATABLES_CSS = ROOT / "resources" / "jquery.dataTables.min.css"
 
@@ -66,19 +69,21 @@ def plot_benchmark(df, kinds=PLOT_KINDS):
 
 def get_results(fnames):
     results = []
-    OUTPUT_BENCH_DIR.mkdir(exist_ok=True, parents=True)
-    OUTPUT_FIG_DIR.mkdir(exist_ok=True, parents=True)
+    BUILD_DIR_OUTPUTS.mkdir(exist_ok=True, parents=True)
+    BUILD_DIR_FIGURES.mkdir(exist_ok=True, parents=True)
 
     for fname in fnames:
         print(f"Processing {fname}")
         fname_path = Path(fname)
         df = pd.read_csv(fname)
+        fname_no_output = fname.replace('outputs/', '')
+        fname_no_output_path = Path(fname_no_output)
 
         # Copy CSV
-        shutil.copy(fname, OUTPUT_BENCH_DIR / fname_path.name)
+        shutil.copy(fname, BUILD_DIR_OUTPUTS / fname_no_output_path)
 
         result = dict(
-            fname_short=fname.replace('outputs/', ''),
+            fname_short=fname.replace('outputs/', '').replace('/', '_'),
             fname=fname,
             fig_fnames=[]
         )
@@ -88,7 +93,7 @@ def get_results(fnames):
 
         for k, fig in enumerate(figs):
             fig_basename = f"{result['fname_short']}_{k}.svg"
-            save_name = OUTPUT_FIG_DIR / fig_basename
+            save_name = BUILD_DIR_FIGURES / fig_basename
             fig.savefig(save_name)
             result['fig_fnames'].append(f'figures/{fig_basename}')
             plt.close(fig)
@@ -102,7 +107,7 @@ def get_results(fnames):
     return results
 
 
-def render_index(results):
+def render_benchmark(results, benchmark):
     cssclass = "highlight"
     formatter = HtmlFormatter(cssclass=cssclass)
 
@@ -112,8 +117,10 @@ def render_index(results):
         + "\n"
         + JQUERY_DATATABLES_JS.read_text("utf-8")
     )
-    return Template(filename=str(TEMPLATE), input_encoding="utf-8").render(
+    return Template(filename=str(TEMPLATE_BENCHMARK),
+                    input_encoding="utf-8").render(
         results=results,
+        benchmark=benchmark,
         code_formatter_css=formatter.get_style_defs(f'.{cssclass}'),
         table_formatter_css=table_formatter_css,
         table_formatter_js=table_formatter_js,
@@ -123,14 +130,36 @@ def render_index(results):
     )
 
 
+def render_index(benchmarks):
+    cssclass = "highlight"
+    formatter = HtmlFormatter(cssclass=cssclass)
+
+    table_formatter_css = JQUERY_DATATABLES_CSS.read_text("utf-8")
+    table_formatter_js = (
+        JQUERY_JS.read_text("utf-8")
+        + "\n"
+        + JQUERY_DATATABLES_JS.read_text("utf-8")
+    )
+    return Template(filename=str(TEMPLATE_INDEX),
+                    input_encoding="utf-8").render(
+        benchmarks=benchmarks,
+        code_formatter_css=formatter.get_style_defs(f'.{cssclass}'),
+        table_formatter_css=table_formatter_css,
+        table_formatter_js=table_formatter_js,
+        nb_total_benchs=len(benchmarks),
+        max_rows=15,
+        last_updated=datetime.now(),
+    )
+
+
 def copy_static():
-    dst = OUTPUT_DIR / 'static'
+    dst = BUILD_DIR / 'static'
     if dst.exists():
         shutil.rmtree(dst)
     shutil.copytree(ROOT / 'static', dst)
 
 
-def render_all_results(results):
+def render_all_results(results, benchmark):
     cssclass = "highlight"
     formatter = HtmlFormatter(cssclass=cssclass)
 
@@ -140,6 +169,7 @@ def render_all_results(results):
             filename=str(TEMPLATE_RESULT),
             input_encoding="utf-8").render(
             result=result,
+            benchmark=benchmark,
             code_formatter_css=formatter.get_style_defs(f'.{cssclass}'),
         )
         htmls.append(html)
@@ -150,28 +180,50 @@ def render_all_results(results):
 @click.option('--pattern', '-k', 'patterns',
               metavar="<pattern>", multiple=True, type=str,
               help="Include results matching <pattern>.")
-def main(patterns=None):
+@click.option('--benchmark', '-b', 'benchmarks',
+              metavar="<pattern>", multiple=True, type=str,
+              help="benchmarks to include.")
+def main(patterns=(), benchmarks=()):
+    if not benchmarks:
+        benchmarks = [
+            f.name for f in (ROOT / 'outputs').iterdir() if f.is_dir()
+        ]
     if not patterns:
         patterns = ['*']
-    fnames = []
-    for p in patterns:
-        fnames += glob.glob(f'outputs/{p}.csv')
-    fnames = sorted(set(fnames))
-    results = get_results(fnames)
-    rendered = render_index(results)
+
     copy_static()
 
-    index_filename = OUTPUT_DIR / "index.html"
-    print(f"Writing results to {index_filename}")
+    rendered = render_index(benchmarks)
+    index_filename = BUILD_DIR / 'index.html'
+    print(f"Writing index to {index_filename}")
     with open(index_filename, "w") as f:
         f.write(rendered)
 
-    htmls = render_all_results(results)
-    for result, html in zip(results, htmls):
-        result_filename = OUTPUT_DIR / result['page']
-        print(f"Writing results to {result_filename}")
-        with open(result_filename, "w") as f:
-            f.write(html)
+    for benchmark in benchmarks:
+        print(f'Rendering benchmark: {benchmark}')
+
+        Path(BUILD_DIR / benchmark).mkdir(exist_ok=True)
+        Path(BUILD_DIR_FIGURES / benchmark).mkdir(exist_ok=True, parents=True)
+        Path(BUILD_DIR_OUTPUTS / benchmark).mkdir(exist_ok=True, parents=True)
+
+        fnames = []
+        for p in patterns:
+            fnames += glob.glob(f'outputs/{benchmark}/{p}.csv')
+        fnames = sorted(set(fnames))
+        results = get_results(fnames)
+        rendered = render_benchmark(results, benchmark)
+
+        benchmark_filename = BUILD_DIR / f"{benchmark}.html"
+        print(f"Writing {benchmark} results to {benchmark_filename}")
+        with open(benchmark_filename, "w") as f:
+            f.write(rendered)
+
+        htmls = render_all_results(results, benchmark)
+        for result, html in zip(results, htmls):
+            result_filename = BUILD_DIR / result['page']
+            print(f"Writing results to {result_filename}")
+            with open(result_filename, "w") as f:
+                f.write(html)
 
 
 if __name__ == "__main__":
